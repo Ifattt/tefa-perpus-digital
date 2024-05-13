2410377fddb6366d0cb6a3befe7b5eefbaa51a09
Ifattt <142280465+Ifattt@users.noreply.github.com>
2024-05-13 14:14:16 +0700

    Update index.vue

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -4,9 +4,11 @@
       <div class="col-lg-12">
         <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
         <div class="my-3">
-          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
+          <form @submit.prevent="getPengunjung">
+          <input v-model="keyword" type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
+          </form>
         </div>
-        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <div class="my-3 text-muted">menampilkan {{ visitors.length }} dari {{ visitor}}</div>
         <table class="table">
           <thead>
             <tr>
@@ -18,7 +20,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(visitor,i) in visitor" :key="i">
+            <tr v-for="(visitor,i) in visitors" :key="i">
               <td>{{ i+1 }}.</td>
               <td>{{ visitor.nama }}</td>
               <td>{{ visitor.keanggotaan.nama }}</td>
@@ -37,14 +39,21 @@
 
 
 <script setup>
-const supabase = useSupabaseClient()
-const visitor = ref([])
-const getPengunjung = async () => {
-  const { data, error } = await supabase.from('pengunjung').select('*, keanggotaan(*), keperluan(*)')
-  if(data) visitor.value = data
-}
+const supabase = useSupabaseClient();
+const visitor = ref(0);
+const visitors=ref([]);
+const keyword = ref([]);
 
+const getPengunjung = async () => {
+  const { data, error } = await supabase.from('pengunjung').select('*, keanggotaan(*), keperluan(*)').ilike(`nama`, `%${keyword.value}%`).order("id", { ascending: false });
+  if(data) visitors.value = data
+}
+const hitungData = async () => {
+  const { data, count } = await supabase.from('pengunjung').select("*", { count : 'exact'})
+  if(data) visitor.value = count
+}
 onMounted(() => {
-  getPengunjung ()
+  getPengunjung();
+  hitungData();
 })
 </script>
